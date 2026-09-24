aba622ac4d30d90874d38cfbf759796835b21ccf
gpoojitha290823-sudo <gpoojitha290823@gmail.com>
2026-09-24 21:29:53 +0530

    practiced Python arithmetic operators examples

---
+++ practice.py
@@ -0,0 +1,53 @@
+print("Hello world")
+
+#Arithmetic operates
+#(Addition)
+a = 2026
+b = 2030
+c = a+b
+print(c)
+
+#Substraction
+x = 2579
+y = 9752
+z = x-y
+print(z)
+
+#multiplication
+m = 2024
+n = 2025
+p = m*n
+print(p)
+
+#divison
+a = 1024
+b = 2
+c = a/b
+print(c)
+
+#Floor division
+q = 1947
+w = 4
+e = q//w
+print(e)
+
+#Modulus
+z = 29
+x = 3
+c = z%x
+print(c)
+
+#Exponentiation
+m = 10
+n = 5
+b = m**n
+print(b)
+-----------------------------------------------------------------
+Hello world
+4056
+-7173
+4098600
+512.0
+486
+2
+100000
