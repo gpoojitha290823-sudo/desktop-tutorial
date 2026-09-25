be99b185b343b8d37a29520486118d8dc3107aa5
gpoojitha290823-sudo <gpoojitha290823@gmail.com>
2026-09-25 20:46:19 +0530

    Added python comparison operators examples

---
+++ Python.py
@@ -0,0 +1,64 @@
+#Comparison operators
+
+#Less than(<)
+a = 2026
+b = 2030
+print(a<b)
+
+l = 100
+k = 50
+print(l<k)
+
+#Greater than(>)
+m = 369
+n = 966
+print(m>n)
+
+q = 46575
+w = 464979
+print(q>w)
+
+#Not equal to (!=)
+x = 566
+y = 980
+print(x!=y)
+
+r = 87
+t = 88
+print(r!=t)
+
+#Greater than or equal to(>=)
+marks = 66
+print(marks>=70)
+
+pulse = 71
+print(pulse>=50)
+
+#Less rhan or equal to (<=)
+age = 18
+print(age<=25)
+
+points = 100
+print(points<=156)
+
+#Equal to(==)
+name1 = "Poojitha"
+name2 = "Poojitha"
+print(name1==name2)
+
+name1 = "vivo"
+name2 = "Realme"
+print(name1==name2)
+-------------------------------------------------------------------------------------
+True
+False
+False
+False
+True
+True
+False
+True
+True
+True
+True
+False
